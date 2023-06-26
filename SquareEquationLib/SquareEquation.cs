ef73135908d20c15f604aaf026bad017600b4045
Vlszgr <vlszgr@mail.ru>
2023-06-26 16:05:42 +0000

    test2

--- SquareEquationLib/SquareEquation.cs
+++ SquareEquationLib/SquareEquation.cs
@@ -1,29 +1,47 @@
-﻿namespace SquareEquationLib;
+﻿﻿namespace SquareEquationLib;
 
 public class SquareEquation
 {
     public static double[] Solve(double a, double b, double c)
     {
-        double[] ans=new double[2];
-        double eps=Math.Pow(10,-9);
-        if (Math.Abs(a)<eps){
+        double eps = 0.000000001;
+        double[] answer;
+
+        if (Math.Abs(a) < eps)
+        {
             throw new System.ArgumentException();
         }
-        if (double.IsNaN(a) || double.IsInfinity(a) || double.IsNaN(b) || double.IsInfinity(b) || double.IsNaN(c) || double.IsInfinity(c)){
-            throw new System.ArgumentException();
+
+        foreach (double x in new double[] { a, b, c })
+        {
+            if (double.IsNaN(x) || double.IsInfinity(x))
+            {
+                throw new System.ArgumentException();
+            }
         }
-        double d=Math.Pow(b,2)-4*a*c;
-        if (d<0 && !(Math.Abs(d)<eps) ){
-            ans=new double[0];
+
+        double d = Math.Pow(b, 2) - 4 * a * c;
+
+        if (d < 0 && !(Math.Abs(d) < eps))
+        {
+            answer = new double[0];
+
         }
-        else if (d < eps){
-            ans=new double[1];
-            ans[0]=-(b+Math.Sign(b)*Math.Sqrt(d))/2;
+        else if (d < eps)
+        {
+            answer = new double[1];
+            answer[0] = -(b + Math.Sign(b) * Math.Sqrt(d)) / 2;
+
         }
-        else{
-            ans[0]=-(b+Math.Sign(b)*Math.Sqrt(d))/2;
-            ans[1]=c/ans[0];
+        else
+        {
+            answer = new double[2];
+            answer[0] = -(b + Math.Sign(b) * Math.Sqrt(d)) / 2;
+            answer[1] = c / answer[0];
+
         }
-        return ans;
+
+        return answer;
+
     }
 }
