4503bc40433ec038056ffbd5cff3f1931c037bc0
Vlszgr <vlszgr@mail.ru>
2023-06-28 19:08:29 +0000

    test

--- SquareEquation.Tests/SquareEquation_Test.cs
+++ SquareEquation.Tests/SquareEquation_Test.cs
@@ -3,48 +3,42 @@ using SquareEquationLib;
 
 namespace SquareEquation_Test
 {
-    public class SquareEquation_Test
+    public class UnitTest1
+{
+    [Fact]
+    public void TwoRoots()
     {
-        [Theory]
-        [InlineData(double.NaN,1,1)]
-        [InlineData(1,double.NaN,1)]
-        [InlineData(1,1,double.NaN)]
-
-        [InlineData(double.PositiveInfinity,1,1)]
-        [InlineData(1,double.PositiveInfinity,1)]
-        [InlineData(1,1,double.PositiveInfinity)]
-
-        [InlineData(double.NegativeInfinity,1,1)]
-        [InlineData(1,double.NegativeInfinity,1)]
-        [InlineData(1,1,double.NegativeInfinity)]
-
-        public void ThrowError(double a,double b, double c)
-        {
-            Assert.Throws<ArgumentException>(() => SquareEquation.Solve(a, b, c));
-        }
-
-        [Theory]
-        [InlineData(1, 6, 9, new double[]{-3})]
-        [InlineData(10, 2, 10, new double[] {})]
-        [InlineData(1,2,-3, new double[] {1, -3})]
-        [InlineData(1, 0, 1, new double[]{})]
-        [InlineData(1, 4, 5, new double[]{})]
-
-        public void noThrowError(double a, double b, double c, double[] expected)
-        {
-            var result=SquareEquation.Solve(a,b,c);
-            Array.Sort(result);
-            Array.Sort(expected);
-
-            if (expected.Length != result.Length)
-            {
-                Assert.Fail("Amount of roots does not match");
-            }
-
-            for (int i = 0; i < expected.Length; i++)
-            {
-                Assert.Equal(expected[i], result[i], 5);
-            }
-        }
+        double[] Answer = SquareEquation.Solve(1, -5, 4);
+        Array.Sort(Answer);
+        double[] expected = {1.0, 4.0};
+        double eps = Math.Pow(10, -9);
+        Assert.Equal(Answer[0], expected[0], eps);
+        Assert.Equal(Answer[1], expected[1], eps);
     }
+
+    [Fact]
+    public void OneRoot()
+    {
+        double[] Answer = SquareEquation.Solve(1, 2, 1);
+        double[] res = {-1.0};
+        double eps = Math.Pow(10, -9);
+        Assert.Equal(Answer[0], res[0], eps);
+    }
+
+    [Fact]
+    public void NoRoots()
+    {
+        double[] Answer = SquareEquation.Solve(1, 1, 4);
+        Assert.True(Answer.Length == 0);
+    }
+
+    [Theory]
+    [InlineData(0, 4, 1)]
+    [InlineData(1, double.NaN, -4)]
+    [InlineData(1, 4, double.NegativeInfinity)]
+    public void CauseErrors(double value1, double value2, double value3)
+    {
+        Assert.Throws<System.ArgumentException>(() => SquareEquation.Solve(value1, value2, value3));
+    }
+}
 }
