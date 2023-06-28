99c08724cb7884a7bf38d23f6ceb3cb75756fa14
Vlszgr <vlszgr@mail.ru>
2023-06-28 19:06:25 +0000

    test

--- SquareEquation.Tests/SquareEquation_Test.cs
+++ SquareEquation.Tests/SquareEquation_Test.cs
@@ -6,9 +6,17 @@ namespace SquareEquation_Test
     public class SquareEquation_Test
     {
         [Theory]
-        [InlineData(0,1,1,1)]
+        [InlineData(double.NaN,1,1)]
         [InlineData(1,double.NaN,1)]
+        [InlineData(1,1,double.NaN)]
+
+        [InlineData(double.PositiveInfinity,1,1)]
+        [InlineData(1,double.PositiveInfinity,1)]
         [InlineData(1,1,double.PositiveInfinity)]
+
+        [InlineData(double.NegativeInfinity,1,1)]
+        [InlineData(1,double.NegativeInfinity,1)]
+        [InlineData(1,1,double.NegativeInfinity)]
 
         public void ThrowError(double a,double b, double c)
         {
@@ -16,9 +24,11 @@ namespace SquareEquation_Test
         }
 
         [Theory]
-        [InlineData(2, 2, 3, new double[0])]
-        [InlineData(1, 2, 1, new double[1] {-1})]
-        [InlineData(1,2,-3, new double[2] {1, -3})]
+        [InlineData(1, 6, 9, new double[]{-3})]
+        [InlineData(10, 2, 10, new double[] {})]
+        [InlineData(1,2,-3, new double[] {1, -3})]
+        [InlineData(1, 0, 1, new double[]{})]
+        [InlineData(1, 4, 5, new double[]{})]
 
         public void noThrowError(double a, double b, double c, double[] expected)
         {
