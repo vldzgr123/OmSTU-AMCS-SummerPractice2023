a9241bfe179ffdc497556c7cbbe7de933e5418bd
Vlszgr <vlszgr@mail.ru>
2023-07-05 05:03:06 +0000

    cadets

---
+++ SpaceCadets/Program.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+var Cadets=new List<Cadet>();
+string pathInp = args[0];
+string pathOut = args[1];
+JObject readFile = JObject.Parse(File.ReadAllText(pathInp));
+
+class Cadet
+{
+    public string Name {get; set;} = "Undefined";
+    public string Group {get; set;} = "Undefined";
+    public string Discipline {get; set;} = "Undefined";
+    public int Mark {get; set;}
+}
