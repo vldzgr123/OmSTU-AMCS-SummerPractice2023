349d62618858ca507d7e9c0ad1339194b8f937b8
Vlszgr <vlszgr@mail.ru>
2023-07-05 12:05:42 +0000

    pool

---
+++ SpaceShip/Ship.cs
@@ -0,0 +1,55 @@
+﻿using SpaceBattle;
+
+namespace SpaceShip1;
+
+public class Pool
+{
+    private Queue<SpaceShip> Ships;
+    public Pool(int count)
+    {
+        Ships = new Queue<SpaceShip>(count);
+        for (int i = 0; i < Ships.Count; i++)
+            Ships.Enqueue(new SpaceShip());
+    }
+    public SpaceShip TakeSlot()
+    {
+        if (Ships.Count() > 0)
+            return Ships.Dequeue();
+        else
+            throw new Exception();
+    }
+    public void Release(SpaceShip ship)
+    {
+        Ships.Enqueue(ship);
+    }
+}
+public class PoolSafety : IDisposable
+{
+    private Pool Pool;
+    private SpaceBattle.SpaceShip Ship;
+    public SpaceBattle.SpaceShip Object()
+    {
+        return Ship;
+    }
+    public PoolSafety(Pool pool)
+    {
+        this.Pool = pool;
+        Ship = pool.TakeSlot();
+    }
+    public void Dispose()
+    {
+        Pool.Release(Ship);
+    }
+}
+class Programm
+{
+    static void Main(string[] args)
+    {
+        Pool spaceShipPool = new Pool(5);
+        using (PoolSafety guard = new PoolSafety(spaceShipPool))
+        {
+            SpaceShip spaceShip = guard.Object();
+        }
+    }
+}
+
